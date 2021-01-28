961bff7acb08b033669ca09b20b94d77c711b578
arma73 <arma.biz9805@gmail.com>
2021-01-28 17:54:16 +0400

    Update: initialize the apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => res.send("hello world"));
-
-app.get("/listings", (req, res) => {
-    return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-    const id: string = req.body.id;
-
-    for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-            return res.send(listings.splice(i, 1));
-        }
-    };
-
-    return res.send("failed to delete listing");
-});
+const server = new ApolloServer({ });
+server.applyMiddleware({ app, "path": "/api" });
 
 app.listen(port, () => console.log(`[app]: http://localhost:${port}`));
