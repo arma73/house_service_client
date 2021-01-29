4f87c102e7c38cffb6f0809f3747b9b126d68dde
arma73 <arma.biz9805@gmail.com>
2021-01-29 09:32:21 +0400

    create: react app

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+render(
+    <React.StrictMode>
+        <div />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+reportWebVitals();
