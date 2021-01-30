026a8be8ff3ae52f5a0da4bd5a721b45940d2bb4
arma73 <arma.biz9805@gmail.com>
2021-01-30 11:28:21 +0400

    update: include routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import reportWebVitals from "./reportWebVitals";
+import Routes from "./routes/Routes";
+
+import "./temp/styles/index.css";
+
+const client = new ApolloClient({
+    "uri": "/api",
+});
 
 render(
     <React.StrictMode>
-        <div />
+        <ApolloProvider client={client}>
+            <Routes />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
