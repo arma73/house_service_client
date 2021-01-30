1e93298dcefc757cac7344853a1a61e690bb07e2
arma73 <arma.biz9805@gmail.com>
2021-01-30 11:56:56 +0400

    update: listing document structure

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,11 +1,37 @@
 import { Collection, ObjectId } from "mongodb";
 
+export enum ListingType {
+    Apartment = "apartment",
+    House = "house",
+}
+
+export interface BookingsIndexMonth {
+    [key: string]: boolean;
+}
+
+export interface BookingsIndexYear {
+    [key: string]: BookingsIndexMonth;
+}
+
 export interface Booking {
     "_id": ObjectId;
 }
 
 export interface Listing {
     "_id": ObjectId;
+    "title": string;
+    "description": string;
+    "image": string;
+    "host": string;
+    "type": ListingType;
+    "address": string;
+    "country": string;
+    "admin": string;
+    "city": string;
+    "bookings": ObjectId[];
+    "bookingsIndex": BookingsIndexYear;
+    "price": number;
+    "numOfGuests": number;
 }
 
 export interface User {
