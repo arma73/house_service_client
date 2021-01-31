f76446b1e953247e732eae930aa12a5b52b7e513
arma73 <arma.biz9805@gmail.com>
2021-01-31 13:26:44 +0400

    add: the ability to log in with google

--- client/src/sections/logIn/LogIn.tsx
+++ client/src/sections/logIn/LogIn.tsx
@@ -1,37 +1,105 @@
-import React from "react";
-import { Card, Layout, Typography } from "antd";
+import React, { Dispatch, SetStateAction, FC, useEffect, useRef } from "react";
+import { Redirect } from "react-router-dom";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import ErrorBanner from "../../lib/components/errorbanner";
+import { Card, Layout, Typography, Spin } from "antd";
+import { AUTH_URL, AuthUrl as AuthUrlData } from "../../lib/graphql/queries/authUrl/";
+import { LOG_IN, LogInVariables, LogIn as LogInData } from "../../lib/graphql/mutations/logIn";
+import { Viewer } from "../../lib/types";
+import { displayErrorMessage, displaySuccessNotification } from "../../lib/utils/display";
 
 import GoogleLogo from "../../assets/google_logo.jpg";
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
-const LogIn = () => (
-    <Content className="log-in">
-        <Card className="log-in-card">
-            <div className="log-in-card__intro">
-                <Title level={3} className="log-in-card__intro-title">
-                    <span role="img" aria-label="wave">
-                        👋
+interface IProps {
+    setViewer: Dispatch<SetStateAction<Viewer>>;
+}
+
+const LogIn: FC<IProps> = ({ setViewer }) => {
+    const client = useApolloClient();
+    const [
+        logIn, 
+        { "data": logInData, "loading": logInLoading, "error": logInError }
+    ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+        "onCompleted": (data) => {
+            if (data && data.logIn) {
+                setViewer(data.logIn);
+                displaySuccessNotification("You've successfully logged in!");
+            }
+        }
+    });
+    const logInRef = useRef(logIn);
+
+    useEffect(() => {
+        const code = new URL(window.location.href).searchParams.get("code");
+
+        if (code) {
+            logInRef.current({
+                "variables": {
+                    "input": { code },
+                },
+            });
+        }
+    }, []);
+
+    const handleAuthorize = async () => {
+        try {
+            const { data } = await client.query<AuthUrlData>({
+                "query": AUTH_URL,
+            });
+            window.location.href = data.authUrl;
+        } catch {
+            displayErrorMessage("Sorry! We aren't able to log you in. Please try again later!");
+        }
+    }
+
+    if (logInLoading) {
+        return (
+            <Content className="log-in">
+                <Spin size="large" tip="Logging you in..." />
+            </Content>
+        )
+    }
+
+    if (logInData && logInData.logIn) {
+        const { "id": viewerId } = logInData.logIn;
+        return <Redirect to={`/user/${viewerId}`} />;
+    }
+
+    const logInErrorBannerElement = logInError 
+        ? <ErrorBanner description="Sorry! We aren't able to log you in. Please try again later!" />
+        : null;
+
+    return (
+        <Content className="log-in">
+            {logInErrorBannerElement}
+            <Card className="log-in-card">
+                <div className="log-in-card__intro">
+                    <Title level={3} className="log-in-card__intro-title">
+                        <span role="img" aria-label="wave">
+                            👋
+                        </span>
+                    </Title>
+                    <Title level={3} className="log-in-card__intro-title">
+                        Log in to ToHouse!
+                    </Title>
+                    <Text>Sign in with Google to start booking available rentals!</Text>
+                </div>
+                <button className="log-in-card__google-button" onClick={handleAuthorize}>
+                    <img src={GoogleLogo} alt="Google Logo" className="log-in-card__google-button-logo" />
+                    <span className="log-in-card__google-button-text">
+                        Sign in with Google
                     </span>
-                </Title>
-                <Title level={3} className="log-in-card__intro-title">
-                    Log in to ToHouse!
-                </Title>
-                <Text>Sign in with Google to start booking available rentals!</Text>
-            </div>
-            <button className="log-in-card__google-button">
-                <img src={GoogleLogo} alt="Google Logo" className="log-in-card__google-button-logo" />
-                <span className="log-in-card__google-button-text">
-                    Sign in with Google
-                </span>
-            </button>
-            <Text type="secondary">
-                Note: By signing in, you'll be redirected to the Google consent from
-                to sign in with your Google account
-            </Text>
-        </Card>
-    </Content>
-);
+                </button>
+                <Text type="secondary">
+                    Note: By signing in, you'll be redirected to the Google consent from
+                    to sign in with your Google account
+                </Text>
+            </Card>
+        </Content>
+    );
+};
 
 export default LogIn;
