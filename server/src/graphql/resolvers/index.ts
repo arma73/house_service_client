a94fbc1afcfdb3046ccb385bcf24619948a60427
arma73 <arma.biz9805@gmail.com>
2021-01-31 17:12:09 +0400

    add: user graphql initial fields

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,5 @@
 import merge from "lodash.merge";
+import { userResolvers } from "./user";
 import { viewerResolvers } from "./viewer";
 
-export const resolvers = merge(viewerResolvers);
+export const resolvers = merge(userResolvers, viewerResolvers);
