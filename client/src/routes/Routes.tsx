b0ed4e4aa4477d73ce7cbd2fc28ed7636a6c5d71
arma73 <arma.biz9805@gmail.com>
2021-01-31 16:43:06 +0400

    add: viewer cookie on the client

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,6 +1,7 @@
-import React, { SetStateAction, Dispatch, FC } from "react";
+import React, { SetStateAction, Dispatch, FC, useEffect, useRef } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Layout, Affix } from "antd";
+import { Layout, Affix, Spin } from "antd";
+import { useMutation } from "@apollo/react-hooks";
 import { Viewer } from "../lib/types";
 import Header from "../sections/header";
 import Home from "../sections/home";
@@ -10,33 +11,67 @@ import Listings from "../sections/listings";
 import User from "../sections/user";
 import NotFound from "../sections/notfound";
 import LogIn from "../sections/logIn";
+import HeaderSkeleton from "../lib/components/headerskeleton";
+import { LOG_IN, LogIn as LogInData, LogInVariables } from "../lib/graphql/mutations/logIn";
+import ErrorBanner from "../lib/components/errorbanner";
 
 interface IProps {
     "viewer": Viewer;
     "setViewer": Dispatch<SetStateAction<Viewer>>;
 }
 
-const Routes: FC<IProps> = ({ setViewer, viewer }) => (
-    <BrowserRouter>
-        <Layout id="app">
-            <Affix offsetTop={0} className="app__affix-header">
-                <Header viewer={viewer} setViewer={setViewer} />
-            </Affix>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/host" component={Host} />
-                <Route exact path="/listing/:id" component={Listing} />
-                <Route exact path="/listing/:location?" component={Listings} />
-                <Route 
-                    exact 
-                    path="/login" 
-                    render={props => <LogIn {...props} setViewer={setViewer} />} 
-                />
-                <Route exact path="/user/:id" component={User} />
-                <Route component={NotFound} />
-            </Switch>
-        </Layout>
-    </BrowserRouter>
-);
+const Routes: FC<IProps> = ({ setViewer, viewer }) => {
+    const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+        "onCompleted": data => {
+            if (data && data.logIn) {
+                setViewer(data.logIn);
+            }
+        },
+    });
+    const logInRef = useRef(logIn);
+
+    useEffect(() => {
+        logInRef.current();
+    }, []);
+
+    if (!viewer.didRequest && !error) {
+        return (
+            <Layout className="app-skeleton">
+                <HeaderSkeleton />
+                <div className="app-skeleton__spin-section">
+                    <Spin size="large" tip="Launching ToHouse" />
+                </div>
+            </Layout>
+        );
+    }
+
+    const logInErrorBannerElement = error 
+        ? <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later!" />
+        : null;
+
+    return (
+        <BrowserRouter>
+            <Layout id="app">
+                {logInErrorBannerElement}
+                <Affix offsetTop={0} className="app__affix-header">
+                    <Header viewer={viewer} setViewer={setViewer} />
+                </Affix>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/host" component={Host} />
+                    <Route exact path="/listing/:id" component={Listing} />
+                    <Route exact path="/listing/:location?" component={Listings} />
+                    <Route
+                        exact
+                        path="/login"
+                        render={props => <LogIn {...props} setViewer={setViewer} />}
+                    />
+                    <Route exact path="/user/:id" component={User} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+    );
+};
 
 export default Routes;
