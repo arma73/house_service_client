98abda3bc723ee5da3b93feb0c038ea8fd41baa9
arma73 <arma.biz9805@gmail.com>
2021-01-31 04:58:17 +0400

    update: building the ui for login page

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Layout } from "antd";
 import Home from "../sections/home";
 import Host from "../sections/host";
 import Listing from "../sections/listing";
@@ -9,15 +10,17 @@ import LogIn from "../sections/logIn";
 
 const Routes = () => (
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/host" component={Host} />
-            <Route exact path="/listing/:id" component={Listing} />
-            <Route exact path="/listing/:location?" component={Listings} />
-            <Route exact path="/login" component={LogIn} />
-            <Route exact path="/user/:id" component={User} />
-            <Route component={NotFound} />
-        </Switch>
+        <Layout id="app">
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/host" component={Host} />
+                <Route exact path="/listing/:id" component={Listing} />
+                <Route exact path="/listing/:location?" component={Listings} />
+                <Route exact path="/login" component={LogIn} />
+                <Route exact path="/user/:id" component={User} />
+                <Route component={NotFound} />
+            </Switch>
+        </Layout>
     </BrowserRouter>
 );
 
--- client/src/sections/logIn/LogIn.tsx
+++ client/src/sections/logIn/LogIn.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { Card, Layout, Typography } from "antd";
 
-const LogIn = () => <h2> LogIn </h2>;
+import GoogleLogo from "../../assets/google_logo.jpg";
+
+const { Content } = Layout;
+const { Text, Title } = Typography;
+
+const LogIn = () => (
+    <Content className="log-in">
+        <Card className="log-in-card">
+            <div className="log-in-card__intro">
+                <Title level={3} className="log-in-card__intro-title">
+                    <span role="img" aria-label="wave">
+                        👋
+                    </span>
+                </Title>
+                <Title level={3} className="log-in-card__intro-title">
+                    Log in to ToHouse!
+                </Title>
+                <Text>Sign in with Google to start booking available rentals!</Text>
+            </div>
+            <button className="log-in-card__google-button">
+                <img src={GoogleLogo} alt="Google Logo" className="log-in-card__google-button-logo" />
+                <span className="log-in-card__google-button-text">
+                    Sign in with Google
+                </span>
+            </button>
+            <Text type="secondary">
+                Note: By signing in, you'll be redirected to the Google consent from
+                to sign in with your Google account
+            </Text>
+        </Card>
+    </Content>
+);
 
 export default LogIn;
