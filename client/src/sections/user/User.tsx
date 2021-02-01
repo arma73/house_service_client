436ef5ec8786fdc74d2d2f1b67314cfdf192c0c9
arma73 <arma.biz9805@gmail.com>
2021-02-01 13:35:00 +0400

    update: include userlistin & userbookings & add pagination

--- client/src/sections/user/User.tsx
+++ client/src/sections/user/User.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { Col, Layout, Row } from "antd";
 import UserProfile from "./components/userprofile";
+import UserListings from "./components/userlistings";
+import UserBookings from "./components/userbookings";
 import PageSkeleton from "../../lib/components/pageskeleton";
 import ErrorBanner from "../../lib/components/errorbanner";
 import { USER, User as UserData, UserVariables } from "../../lib/graphql/queries/user";
@@ -15,10 +17,18 @@ interface MatchParams {
     "id": string;
 }
 
+const PAGE_LIMIT = 4;
+
 const User: FC<RouteComponentProps<MatchParams> & IProps> = ({ match, viewer }) => {
+    const [listingsPage, setListingsPage] = useState(1);
+    const [bookingsPage, setBookingsPage] = useState(1);
+
     const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
         "variables": {
             "id": match.params.id,
+            "limit": PAGE_LIMIT,
+            bookingsPage,
+            listingsPage,
         }
     });
 
@@ -33,7 +43,7 @@ const User: FC<RouteComponentProps<MatchParams> & IProps> = ({ match, viewer }) 
     if (error) {
         return (
             <Layout.Content>
-                <ErrorBanner 
+                <ErrorBanner
                     description="This user may not exist or we're encountered an error. Please try again soon."
                 />
                 <PageSkeleton />
@@ -43,14 +53,48 @@ const User: FC<RouteComponentProps<MatchParams> & IProps> = ({ match, viewer }) 
 
     const user = data ? data.user : null;
     const isViewerUser = viewer.id === match.params.id;
-    const userProfileElement = user 
-        ? <UserProfile user={user} isViewerUser={isViewerUser} /> 
+
+    const userListings = user
+        ? user.listings
+        : null;
+    const userBookings = user
+        ? user.bookings
+        : null;
+
+    const userProfileElement = user
+        ? <UserProfile user={user} isViewerUser={isViewerUser} />
+        : null;
+
+    const userListingsElement = userListings
+        ? (
+            <UserListings
+                userListings={userListings}
+                listingsPage={listingsPage}
+                limit={PAGE_LIMIT}
+                setListingsPage={setListingsPage}
+            />
+        )
+        : null;
+
+    const userBookingsElement = userBookings
+        ? (
+            <UserBookings
+                userBookings={userBookings}
+                bookingsPage={bookingsPage}
+                limit={PAGE_LIMIT}
+                setBookingsPage={setBookingsPage}
+            />
+        )
         : null;
 
     return (
         <Layout.Content className="user">
             <Row gutter={12} typeof="flex" justify="space-between">
                 <Col xs={24}>{userProfileElement}</Col>
+                <Col xs={24}>
+                    {userListingsElement}
+                    {userBookingsElement}
+                </Col>
             </Row>
         </Layout.Content>
     );
