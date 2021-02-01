4a8b44911568afb2e35f836a64b1b0489e79d7e9
arma73 <arma.biz9805@gmail.com>
2021-02-01 11:46:30 +0400

    create: page skeleton component

---
+++ client/src/lib/components/pageskeleton/PageSkeleton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Skeleton } from "antd";
+
+const PageSkeleton = () => {
+    const skeletonParagraph = (
+        <Skeleton 
+            active
+            paragraph={{ "rows": 4 }}
+            className="page-skeleton__paragraph"
+        />
+    );
+
+    return (
+        <>
+            {skeletonParagraph}
+            {skeletonParagraph}
+            {skeletonParagraph}
+        </>
+    )
+};
+
+export default PageSkeleton;
