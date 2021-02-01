c28a19ff98bf4d00ce347ad9fe0ff8a1d3afe046
arma73 <arma.biz9805@gmail.com>
2021-02-01 13:31:06 +0400

    add: booking & listing resolvers

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,5 +1,12 @@
 import merge from "lodash.merge";
+import { bookingResolvers } from "./booking";
+import { listingResolvers } from "./listing";
 import { userResolvers } from "./user";
 import { viewerResolvers } from "./viewer";
 
-export const resolvers = merge(userResolvers, viewerResolvers);
+export const resolvers = merge(
+    bookingResolvers,
+    listingResolvers,
+    userResolvers,
+    viewerResolvers
+);
