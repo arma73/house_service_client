8a9673e1c721854e87bc8fe5c6a08f2547ebd046
arma73 <arma.biz9805@gmail.com>
2021-02-01 13:31:46 +0400

    add: util format

---
+++ client/src/lib/utils/format.ts
@@ -0,0 +1,7 @@
+export const formatListingPrice = (price: number, round = true) => {
+    const formattedListingPrice = round
+        ? Math.round(price / 100)
+        : price / 100;
+    
+    return `${formattedListingPrice}`;
+};
