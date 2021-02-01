1d40f02f4144e74a81288e4d6a9768c1f7858751
arma73 <arma.biz9805@gmail.com>
2021-02-01 13:32:39 +0400

    create: listingcard component

--- client/src/lib/components/listingcard/ListingCard.tsx
+++ client/src/lib/components/listingcard/ListingCard.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import { Card, Typography } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { formatListingPrice } from "../../utils/format";
+import { iconColor } from "../../theme/color";
 
 interface IProps {
     "listing": {
@@ -16,33 +19,39 @@ interface IProps {
 const { Text, Title } = Typography;
 
 const ListingCard: FC<IProps> = ({ listing }) => {
-    const { address, id, image, numOfGuests, price, title } = listing;
+    const { address, image, numOfGuests, price, title, id } = listing;
 
     return (
-        <Card 
-            hoverable 
-            cover={<div style={{ "backgroundImage": `url(${image})` }} />}
-            className="listing-card__cover-img"
-        >
-            <div className="listing-card__details">
-                <div className="listing-card__description">
-                    <Title level={4} className="listing-card__price">
-                        {price}
-                        <span>/day</span>
-                    </Title>
-                    <Text strong ellipsis className="listing-card__title">
-                        {title}
-                    </Text>
-                    <Text strong ellipsis className="listing-card__address">
-                        {address}
-                    </Text>
+        <Link to={`/listing/${id}`}>
+            <Card 
+                hoverable 
+                cover={
+                    <div 
+                        style={{ "backgroundImage": `url(${image})` }}
+                        className="listing-card__cover-img"
+                    />
+                }
+            >
+                <div className="listing-card__details">
+                    <div className="listing-card__description">
+                        <Title level={4} className="listing-card__price">
+                            {formatListingPrice(price)}
+                            <span>/day</span>
+                        </Title>
+                        <Text strong ellipsis className="listing-card__title">
+                            {title}
+                        </Text>
+                        <Text ellipsis className="listing-card__address">
+                            {address}
+                        </Text>
+                    </div>
+                    <div className="listing-card__dimensions listing-card__dimensions--guests">
+                        <UserOutlined style={{ "color": iconColor }} />
+                        <Text>{numOfGuests} guests</Text>
+                    </div>
                 </div>
-                <div className="listing-card__dimensions listing-card__dimensions--guests">
-                    <UserOutlined />
-                    <Text>{numOfGuests} guests</Text>
-                </div>
-            </div>
-        </Card>
+            </Card>
+        </Link>
     );
 };
 
