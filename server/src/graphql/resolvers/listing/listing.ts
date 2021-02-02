afb455b0c0a0c94064b28e77c16780f1247b2eef
arma73 <arma.biz9805@gmail.com>
2021-02-02 12:45:03 +0400

    update: listings resolvers

--- server/src/graphql/resolvers/listing/listing.ts
+++ server/src/graphql/resolvers/listing/listing.ts
@@ -3,7 +3,13 @@ import { Request } from "express";
 import { ObjectID } from "mongodb";
 import { Listing, Database, User } from "../../../lib/types";
 import { authorize } from "../../../lib/utils/authorize";
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types";
+import { 
+    ListingArgs, 
+    ListingBookingsArgs, 
+    ListingBookingsData, 
+    ListingsArgs, 
+    ListingsData, 
+    ListingsFilter } from "./types";
 
 export const listingResolvers: IResolvers = {
     "Query": {
@@ -28,8 +34,39 @@ export const listingResolvers: IResolvers = {
                 throw new Error(`Failed to query listing: ${error}`);
             }
         },
-        "listings": () => {
-            return "Query.listings";
+        "listings": async (
+            _root: undefined, 
+            { filter, limit, page }: ListingsArgs, 
+            { db }: { db: Database }
+        ): Promise<ListingsData> => {
+            try {
+                const data: ListingsData = {
+                    "total": 0,
+                    "result": [],
+                };
+
+                let cursor = await db.listings.find({});
+
+                if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+                    cursor = cursor.sort({ "price": 1 });
+                }
+
+                if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+                    cursor = cursor.sort({ "price": -1 });
+                }
+
+                // page = 1; limit = 10; cursor starts at 0
+                // page = 2; limit = 10; cursor starts at 10;
+                cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+                cursor = cursor.limit(limit);
+
+                data.total = await cursor.count();
+                data.result = await cursor.toArray();
+
+                return data;
+            } catch (error) {
+                throw new Error(`Failed to query listings: ${error}`);
+            }
         },
     },
     "Listing": {
