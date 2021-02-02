850fdf8c4c5a45ef7197ca7f801e8d4c366e95e8
arma73 <arma.biz9805@gmail.com>
2021-02-02 10:12:17 +0400

    update: include listingcreatebooking component

--- client/src/sections/listing/Listing.tsx
+++ client/src/sections/listing/Listing.tsx
@@ -2,11 +2,13 @@ import React, { FC, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { Layout, Col, Row } from "antd";
+import { Moment } from "moment";
 import PageSkeleton from "../../lib/components/pageskeleton";
 import ErrorBanner from "../../lib/components/errorbanner";
 import { LISTING, ListingVariables, Listing as ListingData } from "../../lib/graphql/queries/listing";
 import ListingDetails from "./components/listingdetails";
 import ListingBookings from "./components/listingbookings";
+import ListingCreateBooking from "./components/listingcreatebooking";
 
 interface MatchParams {
     "id": string;
@@ -16,6 +18,8 @@ const PAGE_LIMIT = 3;
 
 const Listing: FC<RouteComponentProps<MatchParams>> = ({ match }) => {
     const [bookingsPage, setBookingsPage] = useState(1);
+    const [checkInDate, setCheckInDate] = useState<Moment | null>(null);
+    const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null);
     
     const { loading, data, error } = useQuery<ListingData, ListingVariables>(LISTING, {
         "variables": {
@@ -62,12 +66,27 @@ const Listing: FC<RouteComponentProps<MatchParams>> = ({ match }) => {
         )
         : null;
 
+    const listingCreateBookingElement = listing 
+        ? (
+            <ListingCreateBooking 
+                price={listing.price}
+                checkInDate={checkInDate}
+                checkOutDate={checkOutDate}
+                setCheckInDate={setCheckInDate}
+                setCheckOutDate={setCheckOutDate}
+            />
+        )
+        : null;
+    
     return (
         <Layout.Content className="listings">
             <Row gutter={24} justify="space-between">
                 <Col xs={24} lg={14}>
                     {listingDetailsElement}
                     {listingBookingsElement}
+                </Col>
+                <Col xs={24} lg={10}>
+                    {listingCreateBookingElement}
                 </Col>
             </Row>
         </Layout.Content>
