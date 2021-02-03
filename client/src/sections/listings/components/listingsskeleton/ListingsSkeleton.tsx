f57282b09da69463a966d74aa42abd009fb8bdfb
arma73 <arma.biz9805@gmail.com>
2021-02-03 10:25:18 +0400

    create: listings skeleton component

---
+++ client/src/sections/listings/components/listingsskeleton/ListingsSkeleton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { List, Card, Skeleton } from "antd";
+
+import listingLoadingCardCover from "../../../../assets/listing-loading-card-cover.jpg";
+
+const HomeListingsSkeleton = () => {
+    const emptyData = [{}, {}, {}, {}, {}, {}, {}, {}];
+    return (
+        <div className="home-listings-skeleton">
+            <Skeleton paragraph={{ "rows": 1 }} />
+            <List 
+                grid={{
+                    "gutter": 8,
+                    "column": 4,
+                }}
+                dataSource={emptyData}
+                renderItem={() => (
+                    <List.Item>
+                        <Card 
+                            cover={
+                                <div 
+                                    style={{ "backgroundImage": `url(${listingLoadingCardCover})` }}
+                                    className="home-listings-skeleton__card-cover-img"
+                                />
+                            }
+                            loading
+                        />
+                    </List.Item>
+                )}
+            />
+        </div>
+    );
+};
+
+export default HomeListingsSkeleton;
