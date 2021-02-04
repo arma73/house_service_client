6c77cc6733a3156ab74d81faf370fc98a1ef2124
arma73 <arma.biz9805@gmail.com>
2021-02-04 14:01:31 +0400

    add: body parsing middleware & provide limit request bodies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 import express, { Application } from "express";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./db";
@@ -11,6 +12,7 @@ const { PORT, SECRET } = process.env;
 const mount = async (app: Application) => {
     const db = await connectDatabase();
 
+    app.use(bodyParser.json({ "limit": "2mb" }));
     app.use(cookieParser(SECRET));
 
     const server = new ApolloServer({
